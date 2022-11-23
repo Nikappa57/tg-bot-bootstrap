762708db1201f5f8c4f991fb3a09baaa29a359bb
Nikappa57 <lorenz.gaudino@gmail.com>
2022-11-23 17:50:46 +0100

    private chat_type fix

--- Bot/decorator/chattype.py
+++ Bot/decorator/chattype.py
@@ -12,8 +12,12 @@ def group(fun):
 
 def private(fun):
     def wrapper(update, *args):
-        data = update.message if update.message else update.edited_message
-        
-        if data.chat.type == 'private':
-            fun(update, *args)
+        data = update.message if update.message else update.callback_query \
+                if update.callback_query else update.edited_message
+        if data:
+            if not update.callback_query:
+                if data.chat.type == 'private':
+                    fun(update, *args)
+            else:
+                fun(update, *args)
     return wrapper
